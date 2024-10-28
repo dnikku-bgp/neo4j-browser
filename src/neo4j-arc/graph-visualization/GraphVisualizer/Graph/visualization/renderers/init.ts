134851b1abcfbe30400a1447980a44606ea9e5ac
Nicu Dascalu <nicolae-florin.dascalu@bertelsmann.de>
2024-10-28 10:34:24 +0100

    supports different outline for Nodes: circle, square, triangle, hexagon, diamond, etc.

--- src/neo4j-arc/graph-visualization/GraphVisualizer/Graph/visualization/renderers/init.ts
+++ src/neo4j-arc/graph-visualization/GraphVisualizer/Graph/visualization/renderers/init.ts
@@ -27,6 +27,13 @@ import Renderer from '../Renderer'
 const noop = () => undefined
 
 const nodeRingStrokeSize = 4
+const SQRT2 = Math.sqrt(2)
+const SQRT3 = Math.sqrt(3)
+const SQRT10 = Math.sqrt(10)
+
+const TAN30 = Math.tan(Math.PI / 6)
+const COS30 = Math.cos(Math.PI / 6)
+const WYE_LEN = 2 / Math.sqrt(1 + Math.pow(2 + TAN30, 2))
 
 const borderColor = (color: string, defaultValue: string) =>
   color ? rgb(color).darker() : defaultValue
@@ -37,25 +44,105 @@ const borderColor = (color: string, defaultValue: string) =>
 const nodeOutline = new Renderer<NodeModel>({
   name: 'nodeOutline',
   onGraphChange(selection, viz) {
-    return selection
+    const fill = function (selection2: unknown) {
+      selection2
+        .attr('fill', (node: NodeModel) => viz.style.forNode(node).get('color'))
+        .attr('stroke', (node: NodeModel) => 
+          borderColor(viz.style.forNode(node).props['color'], '#000000')
+        )
+        .attr('stroke-width', (node: NodeModel) => viz.style.forNode(node).get('border-width'))
+    }
+
+    selection
+      .filter(node => viz.style.forNode(node).props['outline'] == 'square')
+      .selectAll('rect.b-outline')
+      .data(node => [node])
+      .join('rect')
+      .classed('b-outline', true)
+      .attr('x', (node: NodeModel) => -node.radius / SQRT2)
+      .attr('y', (node: NodeModel) => -node.radius / SQRT2)
+      .attr('width', (node: NodeModel) => node.radius * 2 / SQRT2)
+      .attr('height', (node: NodeModel) => node.radius * 2 / SQRT2)
+      .call(fill)
+
+    selection
+      .filter(node => viz.style.forNode(node).props['outline'] == 'triangle')
+      .selectAll('path.b-outline')
+      .data(node => [node])
+      .join('path')
+      .classed('b-outline', true)
+      .attr('d', (node: NodeModel) => {
+        const r = node.radius
+        const l = SQRT3 * r
+        return `M 0 ${-r} L ${-l/2} ${r/2} L ${l/2} ${r/2} Z`
+      })
+      .call(fill)
+    
+    selection
+      .filter(node => viz.style.forNode(node).props['outline'] == 'hexagon')
+      .selectAll('path.b-outline')
+      .data(node => [node])
+      .join('path')
+      .classed('b-outline', true)
+      .attr('d', (node: NodeModel) => {
+        const r = node.radius
+        const l = SQRT3 * r
+        return `M 0 ${-r} L ${-l/2} ${-r/2} L ${-l/2} ${r/2} L 0 ${r} L ${l/2} ${r/2} L ${l/2} ${-r/2} Z`
+      })
+      .call(fill)
+    
+    selection
+      .filter(node => viz.style.forNode(node).props['outline'] == 'cross')
+      .selectAll('path.b-outline')
+      .data(node => [node])
+      .join('path')
+      .classed('b-outline', true)
+      .attr('d', (node: NodeModel) => {
+        const l = node.radius * 2 / SQRT10
+        return `M ${-l/2} ${-3*l/2}  l 0 ${l}  l ${-l} 0  l 0 ${l}  l ${l} 0  l 0 ${l}  l ${l} 0`
+          + ` l 0 ${-l}  l ${l} 0  l 0 ${-l}  l ${-l} 0  l 0 ${-l}  Z`
+      })
+      .call(fill)
+    
+    selection
+      .filter(node => viz.style.forNode(node).props['outline'] == 'wye')
+      .selectAll('path.b-outline')
+      .data(node => [node])
+      .join('path')
+      .classed('b-outline', true)
+      .attr('d', (node: NodeModel) => {
+        const l = node.radius * WYE_LEN
+        const l_cos30 = l * COS30
+        
+        return `M ${-l/2} ${-l -l/2 * TAN30} l 0 ${l} l ${-l_cos30} ${l/2}`
+          + ` l ${l/2} ${l_cos30} l ${l_cos30} ${-l/2} l ${l_cos30} ${l/2}`
+          + ` l ${l/2} ${-l_cos30} l ${-l_cos30} ${-l/2} l 0 ${-l} Z`
+      })
+      .call(fill)
+
+    
+    selection
+      .filter(node => viz.style.forNode(node).props['outline'] == 'diamond')
+      .selectAll('path.b-outline')
+      .data(node => [node])
+      .join('path')
+      .classed('b-outline', true)
+      .attr('d', (node: NodeModel) => {
+        const r = node.radius
+        return `M 0 ${-r} L ${-r} ${0} L 0 ${r} L ${r} 0 Z`
+      })
+      .call(fill)
+
+    selection
+      .filter(node => (viz.style.forNode(node).props['outline'] ?? 'circle') == 'circle')
       .selectAll('circle.b-outline')
       .data(node => [node])
       .join('circle')
       .classed('b-outline', true)
       .attr('cx', 0)
       .attr('cy', 0)
-      .attr('r', (node: NodeModel) => {
-        return node.radius
-      })
-      .attr('fill', (node: NodeModel) => {
-        return viz.style.forNode(node).get('color')
-      })
-      .attr('stroke', (node: NodeModel) => {
-        return viz.style.forNode(node).get('border-color')
-      })
-      .attr('stroke-width', (node: NodeModel) => {
-        return viz.style.forNode(node).get('border-width')
-      })
+      .attr('r', (node: NodeModel) => node.radius)
+      .call(fill)
   },
   onTick: noop
 })
